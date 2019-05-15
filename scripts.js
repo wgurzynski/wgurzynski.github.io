2708f62bf48afd1b828dd2f3b2b8cfa954450a0e
wgurzynski <mac@wgurzynski-2.local>
2019-05-15 23:26:12 +0200

    changed colors, and font weight, also some overall styling details

--- scripts.js
+++ scripts.js
@@ -12,3 +12,18 @@ $('nav a').on('click', function(e) {                 // User clicks nav link
   console.log($('#content').load(url + ' #container'));
 });
 
+$('.educationAndSkillsHeader').on('click', function(){
+  $('.educationAndSkillsContainer').hide();
+  console.log(elo);
+})
+
+let educationAndSkillsHeader = document.getElementsByClassName('educationAndSkillsHeader')[0];
+console.log(educationAndSkillsHeader);
+educationAndSkillsHeader.addEventListener('click', function(){
+  let educationAndSkillsContainer = document.getElementsByClassName('educationAndSkillsContainer')[0]
+  educationAndSkillsContainer.style.display = "none";
+},false);
+
+
+
+document.getElementsByClassName('educationAndSkillsHeader')[0].addEventListener('click', function(){console.log('tego')},false);
